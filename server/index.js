e2d4b3911b08d39dbde4cae550f89630da04ef81
grey275 <pjvanderpol@gmail.com>
2019-05-17 05:27:04 -0700

    removed some comments

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
 "use strict";
 
 // Basic express setup:
-
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
-
-// The in-memory database of tweets. It's a basic object with an array in it.
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,22 +11,8 @@ app.use(express.static("public"));
 
 (async () => {
   const db = await require("./lib/load-mongo");
-
-  // The `data-helpers` module provides an interface to the database of tweets.
-  // This simple interface layer has a big benefit: we could switch out the
-  // actual database it uses and see little to no changes elsewhere in the code
-  // (hint hint).
-  //
-  // Because it exports a function that expects the `db` as a parameter, we can
-  // require it and pass the `db` parameter immediately:
-  const DataHelpers =  require("./lib/data-helpers.js")(db);
-
-  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-  // so it can define routes that use it to interact with the data layer.
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-// Mount the tweets routes at the "/tweets" path prefix:
-// console.log(tweetsRoutes);
   app.use("/tweets", tweetsRoutes);
   app.listen(PORT, () => {
     console.log("Tweeter listening on port " + PORT);
